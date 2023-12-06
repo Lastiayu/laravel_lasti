808d83ceff8593573bfdd341e9fbb99c9d380074
Lastiayu <lasti22ti@mahasiswa.pcr.ac.id>
2023-12-06 19:35:53 +0700

    Initial commit

---
+++ laravel_lasti/app/Http/Controllers/SponsorController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sponsor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class SponsorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sponsor = Sponsor::latest()->paginate(10);
+        return view('sponsor.index', compact('sponsor'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sponsor  $sponsor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sponsor $sponsor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sponsor  $sponsor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sponsor $sponsor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sponsor  $sponsor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sponsor $sponsor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sponsor  $sponsor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sponsor $sponsor)
+    {
+        //
+    }
+}
